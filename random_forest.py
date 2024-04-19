c9fc94ff1847e3c3ff5408d0773562d986f51cc7
Nikhitha284 <sleepingkangroo@gmail.com>
2024-04-19 11:47:19 +0530

    ..

--- random_forest.py
+++ random_forest.py
@@ -1,7 +1,112 @@
 import numpy as np
-from decision_trees import DecisionTree
+from collections import Counter
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+        
+    def is_leaf_node(self):
+        return self.value is not None
+
+
+class DecisionTree:
+    def __init__(self, min_samples_split=15, max_depth=3, n_features=None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_features = n_features
+        self.root = None
+
+    def fit(self, X, y, depth=0):  
+        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1], self.n_features)
+        self.root = self._grow_tree(X, y, depth)
+
+    def _grow_tree(self, X, y, depth):
+        n_samples, n_feats = X.shape
+        n_labels = len(np.unique(y))
+
+        if depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split:
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
+        best_feature, best_threshold = self._best_split(X, y, feat_idxs)
+
+        left_idxs, right_idxs = self._split(X.iloc[:, best_feature], best_threshold)
+        left = self._grow_tree(X.iloc[left_idxs, :], y.iloc[left_idxs], depth + 1)
+        right = self._grow_tree(X.iloc[right_idxs, :], y.iloc[right_idxs], depth + 1)
+        return Node(best_feature, best_threshold, left, right)
+
+    def _best_split(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_threshold = None, None
+
+        for feat_idx in feat_idxs:
+            X_column = X.iloc[:, feat_idx]
+            thresholds = np.unique(X_column)
+
+            for thr in thresholds:
+                gain = self._information_gain(y, X_column, thr)
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_threshold = thr
+
+        return split_idx, split_threshold
+
+    def _information_gain(self, y, X_column, threshold):
+        parent_entropy = self._entropy(y)
+        left_idxs, right_idxs = self._split(X_column, threshold)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = self._entropy(y.iloc[left_idxs]), self._entropy(y.iloc[right_idxs])
+        child_entropy = (n_l / n) * e_l + (n_r / n) * e_r
+
+        information_gain = parent_entropy - child_entropy
+        return information_gain
+
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.where(X_column <= split_thresh)[0]
+        right_idxs = np.where(X_column > split_thresh)[0]
+        return left_idxs, right_idxs
+
+    def _entropy(self, y):
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum([p * np.log(p) for p in ps if p > 0])
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        if len(counter) == 0:
+        # If the counter is empty, return a default value (e.g., 0)
+            return 0
+        value = counter.most_common(1)[0][0]
+        return value
+
+
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for _, x in X.iterrows()])
+
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
+
+
 class RandomForest:
     def __init__(self, n_trees=10, min_samples_split=15, max_depth=3, n_features=None):
+        #intialise parameters
         self.n_trees = n_trees
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
